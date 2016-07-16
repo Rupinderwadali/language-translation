41c0a6ecfa8f333d563f8bbc8316d5aef6ea6721
rupinderwadali <rupinderwadali@gmail.com>
2016-07-16 22:21:41 +0530

    Activity log generation

--- app/models/article.rb
+++ app/models/article.rb
@@ -15,9 +15,11 @@
 #
 
 class Article < ActiveRecord::Base
-  include PgSearch
 
-  belongs_to :language
+include PublicActivity::Model
+tracked owner: Proc.new{ |controller, model| controller && controller.current_user }
+
+ belongs_to :language
   belongs_to :category
   has_many :audios
 
@@ -31,18 +33,6 @@ class Article < ActiveRecord::Base
   mount_uploader :picture, PictureUploader
 
   validates :picture, presence: true
-
-  # PgSearch
-  pg_search_scope :article_search,
-                  against: :tsv_data,
-                  using: {
-                      tsearch: {
-                          dictionary: 'english',
-                          any_word: true,
-                          prefix: true,
-                          tsvector_column: 'tsv_data'
-                      }
-                  }
 
   enum state:   [:draft, :published]
 end
--- app/models/category.rb
+++ app/models/category.rb
@@ -9,6 +9,12 @@
 #
 
 class Category < ActiveRecord::Base
+include PublicActivity::Model
+tracked owner: Proc.new{ |controller, model| controller && controller.current_user }
+  
+def self.search(search)
+    where("name LIKE ?", "%#{search}%") 
+  end
   has_many :articles
 
   validates :name, presence: true
